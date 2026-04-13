4471e9f9646eaca2eaff2289f4615fd5eb359e11
Dita <mb.narandita@gmail.com>
2026-04-13 13:27:24 +0700

    submit W09 - 18224026

--- submissions/w09/answers.py
+++ submissions/w09/answers.py
@@ -11,20 +11,24 @@ Format jawaban:
 """
 from __future__ import annotations
 def q01() -> bool:
+    return False
     """[T/F] Jika korelasi antara dua variabel adalah nol, maka kedua variabel tersebut pasti
 independen."""
     raise NotImplementedError
 
 def q02() -> bool:
+    return True
     """[T/F] PDF marginal () didapatkan dengan mengintegralkan (,) terhadap dari − ke .
 X"""
     raise NotImplementedError
 
 def q03() -> bool:
+    return True
     """[T/F] Nilai koefisien korelasi selalu berada di antara -1 dan 1."""
     raise NotImplementedError
 
 def q04() -> str:
+    return "B"
     """[MC] Jika X dan independen, maka E sama dengan:
 
 A) E[X]+E
@@ -34,6 +38,7 @@ D) 0"""
     raise NotImplementedError
 
 def q05() -> str:
+    return "C"
     """[MC] Manakah ukuran yang menunjukkan kekuatan hubungan linear antara dua
 variabel?
 
@@ -44,6 +49,7 @@ D) CDF."""
     raise NotImplementedError
 
 def q06() -> str:
+    return "A"
     """[MC] Jika (X) = 4,() = 9, dan X, independen, maka (X+) adalah:
 
 A) 13
@@ -53,6 +59,7 @@ D) 6,5"""
     raise NotImplementedError
 
 def q07() -> str:
+    return "A"
     """[MC] Fungsi probabilitas kondisional (|) didefinisikan sebagai:
 
 A) (,)/ () X
@@ -62,23 +69,28 @@ D) (,)− () X"""
     raise NotImplementedError
 
 def q08() -> float:
+    return 0.5
     """[Numeric] Jika (X,) = 2, X = 2, = 2, berapakah koefisien korelasinya?"""
     raise NotImplementedError
 
 def q09() -> float:
+    return 30.0
     """[Numeric] Berapakah nilai E jika E[X] = 10 dan E = 20?"""
     raise NotImplementedError
 
 def q10() -> float:
+    return 0.25
     """[Numeric] Jika (,) = 1/4 untuk 0 2 dan 0 2, berapakah P(X 1, 1)?"""
     raise NotImplementedError
 
 def q11() -> float:
+    return 0.3
     """[Numeric] Dalam tabel diskrit, jika P(1,1) = 0,1P(1,2) = 0,2P(2,1) = 0,3P(2,2) = 0,4,
 berapakah probabilitas marginal P(X = 1)?"""
     raise NotImplementedError
 
 def q12() -> float:
+    return 16.0
     """[Numeric] Jika X dan memiliki korelasi 1 dan (X) = 4, berapakah (X+X)?"""
     raise NotImplementedError
 
