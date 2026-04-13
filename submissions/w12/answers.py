1deb19884d00df49d1cea43ba5dd0c2c913ea827
Dita <mb.narandita@gmail.com>
2026-04-13 14:02:39 +0700

    submit W12 - 18224026

--- submissions/w12/answers.py
+++ submissions/w12/answers.py
@@ -11,19 +11,23 @@ Format jawaban:
 """
 from __future__ import annotations
 def q01() -> bool:
+    return False
     """[T/F] Jika p-value lebih kecil dari tingkat signifikansi , maka kita gagal menolak hipotesis
 nol."""
     raise NotImplementedError
 
 def q02() -> bool:
+    return True
     """[T/F] Galat Tipe I adalah kesalahan menolak padahal benar."""
     raise NotImplementedError
 
 def q03() -> bool:
+    return True
     """[T/F] Meningkatkan ukuran sampel biasanya akan meningkatkan kekuatan uji (power)."""
     raise NotImplementedError
 
 def q04() -> str:
+    return "B"
     """[MC] Nilai probabilitas yang menunjukkan kekuatan bukti melawan hipotesis nol disebut:
 
 A) Tingkat kepercayaan.
@@ -33,6 +37,7 @@ D) Parameter."""
     raise NotImplementedError
 
 def q05() -> str:
+    return "C"
     """[MC] Jika kita menguji = 50 vs 50, maka kita melakukan uji:
 0 1
 
@@ -43,6 +48,7 @@ D) Tanpa arah."""
     raise NotImplementedError
 
 def q06() -> str:
+    return "A"
     """[MC] Kondisi di mana kita menolak hipotesis nol padahal sebenarnya salah disebut:
 
 A) Keputusan yang benar (Power).
@@ -52,6 +58,7 @@ D) Signifikansi."""
     raise NotImplementedError
 
 def q07() -> str:
+    return "B"
     """[MC] Tingkat signifikansi yang umum digunakan dalam penelitian adalah:
 
 A) 0,5
@@ -61,24 +68,29 @@ D) 1,0"""
     raise NotImplementedError
 
 def q08() -> float:
+    return 1.0
     """[Numeric] Jika statistik uji = 2,58 dan nilai kritis c = 1,96 untuk uji dua arah, apakah 0
 ditolak? (Tulis 1 untuk Ya, 0 untuk Tidak)"""
     raise NotImplementedError
 
 def q09() -> float:
+    return 0.01
     """[Numeric] Berapakah nilai jika tingkat kepercayaan adalah 99%?"""
     raise NotImplementedError
 
 def q10() -> float:
+    return 9.0
     """[Numeric] Dalam uji t dengan sampel n = 10, berapakah derajat kebebasannya?"""
     raise NotImplementedError
 
 def q11() -> float:
+    return 1.0
     """[Numeric] Jika p-value = 0,02 dan = 0,05, apakah kita menolak 0? (Tulis 1 untuk Ya, 0
 untuk Tidak)"""
     raise NotImplementedError
 
 def q12() -> float:
+    return 1.0
     """[Numeric] Jika statistik = 0, berapakah p-value untuk uji dua arah?"""
     raise NotImplementedError
 
