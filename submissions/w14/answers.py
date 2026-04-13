ec7f3d06252f5d829b4319a384e776bc9af56038
Dita <mb.narandita@gmail.com>
2026-04-13 14:26:56 +0700

    submit W14 - 18224026

--- submissions/w14/answers.py
+++ submissions/w14/answers.py
@@ -11,20 +11,24 @@ Format jawaban:
 """
 from __future__ import annotations
 def q01() -> bool:
+    return True
     """[T/F] Pengujian A/B adalah aplikasi nyata dari uji hipotesis dua sampel."""
     raise NotImplementedError
 
 def q02() -> bool:
+    return True
     """[T/F] Metrik "Presisi" mengukur seberapa banyak dari total prediksi positif yang benar-
 benar positif."""
     raise NotImplementedError
 
 def q03() -> bool:
+    return False
     """[T/F] Dalam monitoring sistem, kita biasanya mengabaikan outlier karena itu bukan
 bagian dari pola normal."""
     raise NotImplementedError
 
 def q04() -> str:
+    return "B"
     """[MC] Metrik evaluasi yang tepat untuk dataset dengan kelas yang tidak seimbang
 (imbalanced) adalah:
 
@@ -35,6 +39,7 @@ D) Range."""
     raise NotImplementedError
 
 def q05() -> str:
+    return "B"
     """[MC] Dalam deteksi anomali, data yang berada di luar 3 biasanya dianggap:
 
 A) Data normal.
@@ -44,6 +49,7 @@ D) Sampel ideal."""
     raise NotImplementedError
 
 def q06() -> str:
+    return "B"
     """[MC] Pengujian A/B dilakukan untuk:
 
 A) Mengurangi biaya server.
@@ -53,6 +59,7 @@ D) Mengganti peran programmer."""
     raise NotImplementedError
 
 def q07() -> str:
+    return "A"
     """[MC] Jika sebuah sistem memiliki presisi 1,0, berarti:
 
 A) Tidak ada false positive.
@@ -62,24 +69,29 @@ D) Sistem sempurna."""
     raise NotImplementedError
 
 def q08() -> float:
+    return 0.8
     """[Numeric] Jika TP = 80 dan FP = 20, berapakah nilai presisinya?"""
     raise NotImplementedError
 
 def q09() -> float:
+    return 950.0
     """[Numeric] Jika akurasi model adalah 0,95 dan ada 1.000 data, berapa banyak prediksi
 yang benar?"""
     raise NotImplementedError
 
 def q10() -> float:
+    return 0.8
     """[Numeric] Hitung F1-score jika Presisi = 0,8 dan Recall = 0,8."""
     raise NotImplementedError
 
 def q11() -> float:
+    return 2.0
     """[Numeric] Berapakah nilai skor-Z untuk data point 110 jika rata-rata 100 dan simpangan
 baku 5?"""
     raise NotImplementedError
 
 def q12() -> float:
+    return 1.0
     """[Numeric] Jika dalam pengujian A/B, p-value yang didapat adalah 0,001, apakah ada
 perbedaan signifikan pada = 0,05? (Tulis 1 untuk Ya, 0 untuk Tidak)"""
     raise NotImplementedError
